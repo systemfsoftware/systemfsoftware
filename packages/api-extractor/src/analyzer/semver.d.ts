a6f69acc30c25de152ab88d5fde5257084e4573c
Ryan Lee <ryan@younna.com>
2026-09-23 12:02:29 +0000

    refactor(api-extractor): declare semver's instance shape as an interface

--- packages/api-extractor/src/analyzer/semver.d.ts
+++ packages/api-extractor/src/analyzer/semver.d.ts
@@ -1,14 +1,8 @@
 declare module 'semver' {
-  export class SemVer {
-    constructor(version: string)
+  export interface SemVer {
+    readonly version: string
   }
-  export class Range {
-    constructor(range: string)
-  }
-  export function minVersion(range: string | Range): SemVer | null
+  export function minVersion(range: string): SemVer | null
   export function validRange(range: string): string | null
   export function gt(v1: string | SemVer, v2: string | SemVer): boolean
-  export function valid(version: string): string | null
-  export function major(version: string): number
-  export function minor(version: string): number
 }
